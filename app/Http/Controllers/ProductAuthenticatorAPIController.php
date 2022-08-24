676c4c2abe3989e9efb55d7584875b2963d8ed88
maTurjo <ma.turjo@gmail.com>
2022-08-25 00:19:03 +0600

    Api endpoint to authenticate License key

---
+++ app/Http/Controllers/ProductAuthenticatorAPIController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ActivatedProduct;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class ProductAuthenticatorAPIController extends Controller
+{
+
+    public function authenticateProduct(Request $req){
+
+        $RequestData=$req->json()->all();
+
+        $licenseKey=$RequestData["LicenseKey"];
+        $verificationStatus=false;
+        $foundLicense=ActivatedProduct::where("activation_key",$licenseKey)->first();
+
+        if($foundLicense){
+            $verificationStatus=true;
+        }
+        return response()
+                ->json(
+                    [
+                        "licenseKey"=>$licenseKey,
+                        "Verification"=>$verificationStatus,
+                    ]
+                );
+    }
+    //
+}
