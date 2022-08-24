2b698cf0b7ac1a0e5208e01e6c268db4270f1f5f
maTurjo <ma.turjo@gmail.com>
2022-08-25 02:40:25 +0600

    Api endpoint to authenticate License key

--- app/Http/Controllers/ProductAuthenticatorAPIController.php
+++ app/Http/Controllers/ProductAuthenticatorAPIController.php
@@ -11,24 +11,37 @@ class ProductAuthenticatorAPIController extends Controller
 
     public function authenticateProduct(Request $req){
 
+        $verificationStatus=false;
         $RequestData=$req->json()->all();
+        if(!isset($RequestData["LicenseKey"])) return response()->json(["errorMessage"=>"error","Verification"=>$verificationStatus]);
 
         $licenseKey=$RequestData["LicenseKey"];
-        $verificationStatus=false;
         $foundLicense=ActivatedProduct::where("activation_key",$licenseKey)->first();
-
         if($foundLicense){
             $verificationStatus=true;
+            return response()
+                    ->json(
+                        [
+                            "licenseKey"=>$licenseKey,
+                            "Verification"=>$verificationStatus,
+                            "ProductName"=>$foundLicense->product->product_name,
+                            "CustomerName"=>$foundLicense->customer->name,
+                            "ActivationAllowed"=>$foundLicense->activation_allowed,
+                            "ActivationDone"=>$foundLicense->activation_done,
+                        ]
+                    );
+        }else{
+            return response()
+                    ->json(
+                        [
+                            "licenseKey"=>$licenseKey,
+                            "Verification"=>$verificationStatus,
+                            "ProductName"=>"",
+                            "CustomerName"=>""
+                        ]
+                    );
+
         }
-        return response()
-                ->json(
-                    [
-                        "licenseKey"=>$licenseKey,
-                        "Verification"=>$verificationStatus,
-                        "ProductName"=>$foundLicense->product->product_name,
-                        "CustomerName"=>$foundLicense->customer->name
-                    ]
-                );
     }
     //
 }
